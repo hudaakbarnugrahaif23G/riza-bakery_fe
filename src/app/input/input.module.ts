1c8b0ea40b048736570a5332df3df6891639338b
Huda Akbar <if23.hudanugraha@mhs.ac.id>
2025-06-20 12:18:19 +0700

    input

--- src/app/input/input.module.ts
+++ src/app/input/input.module.ts
@@ -1,17 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { InputPageRoutingModule } from './input-routing.module';
-
 import { InputPage } from './input.page';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
+    ReactiveFormsModule,
     IonicModule,
     InputPageRoutingModule
   ],
--- src/app/input/input.page.ts
+++ src/app/input/input.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-input',
@@ -6,11 +7,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./input.page.scss'],
   standalone: false,
 })
-export class InputPage implements OnInit {
+export class InputPage {
+  productionForm: FormGroup;
+  isSuccessModalOpen = false;
+  charCount = 0;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private fb: FormBuilder) {
+    this.productionForm = this.fb.group({
+      productionDate: [null, Validators.required],
+      shift: [null, Validators.required],
+      productionLine: [null, Validators.required],
+      productionUnits: [null, [Validators.required, Validators.min(1)]],
+      materialUsed: [null, [Validators.required, Validators.min(0.01)]],
+      productionStatus: [null, Validators.required],
+      processNotes: [''],
+      rejectUnits: [null, Validators.required],
+      qualityLevel: [null, Validators.required],
+    });
   }
 
+  updateCharCount() {
+    this.charCount = this.productionForm.get('processNotes')?.value?.length || 0;
+  }
+
+  submitForm() {
+    if (this.productionForm.valid) {
+      console.log('Data:', this.productionForm.value);
+      this.isSuccessModalOpen = true;
+    }
+  }
+
+  closeSuccessModal() {
+    this.isSuccessModalOpen = false;
+    this.productionForm.reset();
+    this.charCount = 0;
+  }
 }
